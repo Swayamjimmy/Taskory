ec451f060c5550ce0ef5cb46d0454f883b50412e
swayam <swayamwr@gmail.com>
2026-08-19 02:38:00 +0530

    wire the pages together in app

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { DashboardPage } from './pages/DashboardPage';
 import { TaskListPage } from './pages/TaskListPage';
 import { TaskDetailPage } from './pages/TaskDetailPage';
+import { KanbanPage } from './pages/KanbanPage';
 
 // App shell with navigation and routing
 function App() {
@@ -10,11 +11,13 @@ function App() {
       <nav className='bg-gray-800 text-white p-4 flex gap-4'>
         <Link to='/' className='hover:underline'>Dashboard</Link>
         <Link to='/tasks' className='hover:underline'>Tasks</Link>
+        <Link to='/kanban' className='text-white hover:underline'>Kanban</Link>
       </nav>
       <Routes>
         <Route path='/' element={<DashboardPage />} />
         <Route path='/tasks' element={<TaskListPage />} />
         <Route path='/tasks/:id' element={<TaskDetailPage />} />
+        <Route path='/kanban' element={<KanbanPage />} />
       </Routes>
     </BrowserRouter>
   );
